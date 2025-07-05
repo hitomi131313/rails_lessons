bf60e2537e770da756ffd075bc8c406bb7a9eb74
hitomi131313 <214909079+hitomi131313@users.noreply.github.com>
2025-07-05 15:03:05 +0000

    [Finish] 2-5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_07_05_085035) do
+ActiveRecord::Schema.define(version: 2025_07_05_085211) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2025_07_05_085035) do
     t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
   end
 
 end
